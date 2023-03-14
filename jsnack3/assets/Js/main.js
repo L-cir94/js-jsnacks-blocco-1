528963ec679cf235cdaef085ec01d342dffe8567
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-14 12:41:24 +0100

    done

---
+++ jsnack3/assets/Js/main.js
@@ -0,0 +1,23 @@
+
+/* 
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+/* strumenti: 
+
+*/
+
+
+const primoNumero = Number(prompt('inserisci un numero'))
+const secondoNumero = Number(prompt('inserisci un altro numero'))
+const terzoNumero = Number(prompt('inserisci un altro numero'))
+const quartoNumero = Number(prompt('inserisci un altro numero'))
+const quintoNumero = Number(prompt('inserisci un altro numero'))
+const sestoNumero = Number(prompt('inserisci un altro numero'))
+const settimoNumero = Number(prompt('inserisci un altro numero'))
+const ottavoNumero = Number(prompt('inserisci un altro numero'))
+const nonoNumero = Number(prompt('inserisci un altro numero'))
+const decimoNumero = Number(prompt('inserisci un altro numero'))
+
+console.log(primoNumero+secondoNumero+terzoNumero+quartoNumero+quintoNumero+sestoNumero+settimoNumero+ottavoNumero+nonoNumero+decimoNumero)
