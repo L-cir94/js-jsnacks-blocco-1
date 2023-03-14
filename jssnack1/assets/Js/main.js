f959303be63d424653c2e9f86c51b91f9fb4dc33
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-14 11:49:01 +0100

    done

---
+++ jssnack1/assets/Js/main.js
@@ -0,0 +1,19 @@
+
+/* 
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+/* strumenti: 
+
+*/
+let primoNumero = Number(prompt('inserisci un numero'))
+let secondoNumero = Number(prompt('inserisci un altro numero'))
+
+if (primoNumero === secondoNumero) {
+  console.log(`il primo numero ${primoNumero} e il secondo ${secondoNumero} sono uguali`);
+} else if (primoNumero > secondoNumero) {
+  console.log(`il primo numero ${primoNumero} è maggiore del secondo ${secondoNumero}`);
+} else {
+  console.log(`il secondo numero ${secondoNumero} è maggiore del primo ${primoNumero}`);
+}
