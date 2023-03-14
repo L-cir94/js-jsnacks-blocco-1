59c07e24f36e59f18b118712ca7a16de0d47b83d
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-14 12:14:40 +0100

    done

--- jsnack2/assets/Js/main.js
+++ jsnack2/assets/Js/main.js
@@ -11,4 +11,10 @@ Il software stampa prima la parola più corta, poi la parola più lunga.
 const primaParola = prompt('inserisci la prima parola')
 const secondaParola = prompt('inserisci la seconda parola')
 
-//non ho idea di cosa utilizzare
+if (primaParola.length == secondaParola){
+    console.log('le parole hanno la stessa lunghezza')
+} else if(primaParola.length > secondaParola.length){
+    console.log('la prima parola è più lunga')
+} else{
+    console.log('la senconda parola è più lunga')
+}
