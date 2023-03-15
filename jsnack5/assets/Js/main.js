f09b7c119ba75d97b7e25c8ded1435446d9b71cb
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-15 12:52:10 +0100

    change let in const and numb in userNumb

--- jsnack5/assets/Js/main.js
+++ jsnack5/assets/Js/main.js
@@ -3,15 +3,15 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
-let listaNumeri = [
+const listaNumeri = [
 
 ]
 //ciclo for
 for (let i = 0; i < 6; i++) {
-    let numb = Number(prompt('inserisci un numero'))
-    if (numb % 2 == 0) {
-        listaNumeri.push(numb)
-        console.log(numb)
+    let userNumb = Number(prompt('inserisci un numero'))
+    if (userNumb % 2 == 0) {
+        listaNumeri.push(userNumb)
+        console.log(userNumb)
     }
 }
 
