83df7351535444d8537175b910d5e637cd274856
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-15 12:22:30 +0100

    i don't know how process the prompt with for and then with while

---
+++ jsnack4/assets/Js/main.js
@@ -0,0 +1,21 @@
+/* 
+(sia con for che con while):
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
+chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+*/
+
+const userName = prompt("inserisci il tuo nome")
+
+const invitati = [
+'ciccio',
+'topolino',
+'pluto',
+'mimmo'
+
+]
+
+for (let i = 0; i < invitati.length; i++) {
+    const element = invitati[i];
+    
+}
+/* document.getElementById("demo").innerHTML = */
